df82a33c58438a4c21e4aae64536456f7824a372
Adrián Martínez <4281885@alu.murciaeduca.es>
2024-11-21 09:45:08 +0100

    arreglos

--- resources/views/curriculos/edit.blade.php
+++ resources/views/curriculos/edit.blade.php
@@ -10,28 +10,24 @@
          </div>
          <div class="card-body" style="padding:30px">
 
-            <form action="action([App\Http\Controllers\CurriculoController::class, 'getEdit'], ['id' => $id])`" method="POST">
+            <form action="{{ action([App\Http\Controllers\CurriculoController::class, 'getEdit'], ['id' => $id]) }}" method="POST">
                 @method('PUT')
 	            @csrf
 
 	            <div class="form-group">
-	               <label for="user_id">Curriculo ID</label>
-	               <input type="number" name="user_id" id="user_id" class="form-control" value="{{ $curriculos['user_id'] }}">
+	               <label for="usuario_id">ID Usuario</label>
+	               <input type="number" name="usuario_id" id="usuario_id">
 	            </div>
 
 	            <div class="form-group">
-	               <label for="video_currículum">URL Videocurrículo</label>
-	               <input type="url" name="video_currículum" id="video_currículum" value="{{ $curriculos['video_currículum'] }}">
+	               <label for="video_currículum">Video Currículo</label>
+	               <input type="url" name="video_curriculum" id="video_curriculum" class="form-control">
 	            </div>
 
 	            <div class="form-group">
-	               <label for="texto_curriculum">Texto del currículo</label>
-	               <textarea name="texto_curriculum" id="texto_curriculum" class="form-control" rows="3">
-                    value="{{ $curriculos['texto_curriculum'] }}"
-                   </textarea>
-                   <br /><small>Cada metadato irá separado del siguiente por una línea <br />
-                   y la clave irá separada por : del valor</small>
-	            </div>
+                    <label for="texto_curriculum">Skills</label>
+                    <input type="text" name="texto_curriculum" id="texto_curriculum" class="form-control">
+                 </div>
 
 	            <div class="form-group text-center">
 	               <button type="submit" class="btn btn-primary" style="padding:8px 100px;margin-top:25px;">
@@ -47,5 +43,3 @@
 </div>
 
 @endsection
-
-
