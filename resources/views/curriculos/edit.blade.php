3d7b0e13ce6363d07985f7afa6f760524a1cd2c9
Adrián Martínez <4281885@alu.murciaeduca.es>
2024-11-21 09:41:55 +0100

    arreglos

--- resources/views/curriculos/edit.blade.php
+++ resources/views/curriculos/edit.blade.php
@@ -15,18 +15,20 @@
 	            @csrf
 
 	            <div class="form-group">
-	               <label for="nombre">Estudiante</label>
-	               <input type="number" name="user_id" id="user_id" class="form-control">
+	               <label for="user_id">Curriculo ID</label>
+	               <input type="number" name="user_id" id="user_id" class="form-control" value="{{ $curriculos['user_id'] }}">
 	            </div>
 
 	            <div class="form-group">
-	               <label for="docente_id">URL Videocurrículo</label>
-	               <input type="url" name="video_currículum" id="video_currículum">
+	               <label for="video_currículum">URL Videocurrículo</label>
+	               <input type="url" name="video_currículum" id="video_currículum" value="{{ $curriculos['video_currículum'] }}">
 	            </div>
 
 	            <div class="form-group">
-	               <label for="metadatos">Texto del currículo</label>
-	               <textarea name="texto_curriculum" id="texto_curriculum" class="form-control" rows="3"></textarea>
+	               <label for="texto_curriculum">Texto del currículo</label>
+	               <textarea name="texto_curriculum" id="texto_curriculum" class="form-control" rows="3">
+                    value="{{ $curriculos['texto_curriculum'] }}"
+                   </textarea>
                    <br /><small>Cada metadato irá separado del siguiente por una línea <br />
                    y la clave irá separada por : del valor</small>
 	            </div>
